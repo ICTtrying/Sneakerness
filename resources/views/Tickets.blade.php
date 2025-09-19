b6eaf259b68436733f04e1e34bdace85cf24a744
wesley <340308@student.mboutrecht.nl>
2025-09-19 22:16:18 +0200

    navbar

--- resources/views/Tickets.blade.php
+++ resources/views/Tickets.blade.php
@@ -1,3 +1,15 @@
-<x-Navbar>
-    test 
-</x-Navbar>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <x-Navbar />
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+</body>
+</html>
