1dfdac1dc5286f55381aff4e055082f21714e2c1
AshDashTooFast3 <338127@student.mboutrecht.nl>
2025-09-22 16:03:26 +0200

    Homepagina gefixed

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Sneakerness</title>
+
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="">
+
+<x-navbar />
+<div class="bg-gray-900">
+    <div class="relative isolate px-6 pt-14 lg:px-8">
+        <div aria-hidden="true" class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+            <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"></div>
+        </div>
+        <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+            <div class="text-center">
+                <h1 class="text-5xl font-bold tracking-tight text-balance text-white sm:text-7xl">{{ $home['title'] }}</h1>
+                <p class="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">{{ $home['content'] }}</p>
+                <div class="mt-10 flex items-center justify-center gap-x-6">
+                    <a href="/Tickets" class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Tickets</a>
+                    <a href="/Events" class="text-sm/6 font-semibold text-white">Events <span aria-hidden="true">→</span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+</body>
+</html>
--- sneakerness-app/stands-overzicht/storage/framework/views/f5235a51abf17ea866fe2b9bf3eba17c.php
+++
@@ -1,1 +0,0 @@
-components.app-logo<?php /**PATH C:\phpprojects\Sneakerness\sneakerness-app\stands-overzicht\storage\framework\views/d5ad50570913e1cb5fb33d9f78eba7bf.blade.php ENDPATH**/ ?>
