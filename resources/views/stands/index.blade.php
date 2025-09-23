7270eb12f222da340860860512756937f79416ba
AshDashTooFast3 <338127@student.mboutrecht.nl>
2025-09-23 17:05:19 +0200

    Stands overzicht gemaakt

--- resources/views/stands/index.blade.php
+++ resources/views/stands/index.blade.php
@@ -6,36 +6,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
 </head>
-<body>
-    
-    <div class="container my-5">
-        <h1>{{ $title }}</h1>
-         <p>{{ $message }}</p>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>VerkoperId</th>
-                <th>StandType</th>
-                <th>Prijs</th>
-                <th>VerhuurdStatus</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($stands as $stand)
-                <tr>
-                    <td>{{ $stand->VerkoperId }}</td>
-                    <td>{{ $stand->StandType }}</td>
-                    <td>€{{ $stand->Prijs }}</td>
-                    <td>{{ $stand->VerhuurdStatus == 1 ? 'Verhuurd' : 'Beschikbaar' }}</td>
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="3">Geen stands gevonden.</td>
-                </tr>
-            @endforelse
-        </tbody>
-    </table>
+<body class="bg-gray-900 ">
+    <x-navbar />
+
+      <div class="space-y-6 m-6">
+        <p class="text-gray-700 dark:text-gray-200">
+            <strong class="text-4xl">{{ $title }}</strong>
+            <?php echo str_repeat("<br>", 4); ?>
+            {{ $message }}
+        </p>
+
+        {{-- Example table of vendors --}}
+        <div class="overflow-x-auto">
+            <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                <thead class="bg-gray-50 dark:bg-gray-700">
+                    <tr>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-300">Naam</th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-300">Categorie</th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-300">Stand Type</th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-300">Dagen</th>
+                    </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
+                    @forelse ($stands as $stand)
+                    <tr>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-200">{{ $stand->VerkoperNaam }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-200">{{ $stand->StandType }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-200">€{{ $stand->Prijs }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-200">{{ $stand->VerhuurdStatus == 1 ? 'Verhuurd' : 'Beschikbaar' }}</td>
+                    </tr>
+                    @empty
+                    <tr>
+                        <td colspan="4" class="px-6 py-4 text-sm text-gray-900 dark:text-gray-200 text-center">Geen stands gevonden</td>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
     </div>
 </body>
 </html>
-
