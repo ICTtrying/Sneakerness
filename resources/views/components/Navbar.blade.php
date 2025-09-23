d4e8bb3aba15e819cbca5e748fc75bb6eed2df88
wesley <340308@student.mboutrecht.nl>
2025-09-23 22:09:27 +0200

    updated navbar

--- resources/views/components/Navbar.blade.php
+++ resources/views/components/Navbar.blade.php
@@ -42,14 +42,14 @@
                     <div class="hidden sm:ml-6 sm:block">
                         <div class="flex space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                            <a href="#" aria-current="page"
-                                class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Dashboard</a>
-                            <a href="#"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Team</a>
-                            <a href="#"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Projects</a>
-                            <a href="#"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Calendar</a>
+                            <a href="/" aria-current="page"
+                                class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Home</a>
+                            <a href="/Stands"
+                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Stands</a>
+                            <a href="/Evenementen"
+                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Evenementen</a>
+                            <a href="/Tickets"
+                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Buy Tickets</a>
                         </div>
                     </div>
                 </div>
@@ -96,14 +96,14 @@
         <el-disclosure id="mobile-menu" hidden class="block sm:hidden">
             <div class="space-y-1 px-2 pt-2 pb-3">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                <a href="#" aria-current="page"
-                    class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white">Dashboard</a>
-                <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Team</a>
-                <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Projects</a>
-                <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Calendar</a>
+                <a href="/" aria-current="page"
+                    class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white">Home</a>
+                <a href="/Stands"
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Stand</a>
+                <a href="/Evenementen"
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Evenementen</a>
+                <a href="/Tickets"
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Buy Tickets</a>
             </div>
         </el-disclosure>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketController;
 
 // Nu luistert de route op /
-Route::get('/', [TicketController::class, 'index']);
+Route::get('/Tickets', [TicketController::class, 'index']);
 
 Route::get('/Tickets/{EventTicket}', [TicketController::class, 'Ticketskinds']);
 Route::post('/add-to-cart', [TicketController::class, 'addToCart']);
