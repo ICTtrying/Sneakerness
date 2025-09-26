44611b03cede040834d2699b3c28b036491dd57f
AshDashTooFast3 <338127@student.mboutrecht.nl>
2025-09-26 09:43:35 +0200

    github fixen

--- vendor/phpunit/php-code-coverage/src/Report/Cobertura.php
+++ vendor/phpunit/php-code-coverage/src/Report/Cobertura.php
@@ -12,22 +12,22 @@ namespace SebastianBergmann\CodeCoverage\Report;
 use const DIRECTORY_SEPARATOR;
 use function basename;
 use function count;
+use function dirname;
+use function file_put_contents;
 use function preg_match;
 use function range;
+use function str_contains;
 use function str_replace;
 use function time;
 use DOMImplementation;
 use SebastianBergmann\CodeCoverage\CodeCoverage;
+use SebastianBergmann\CodeCoverage\Driver\WriteOperationFailedException;
 use SebastianBergmann\CodeCoverage\Node\File;
 use SebastianBergmann\CodeCoverage\Util\Filesystem;
-use SebastianBergmann\CodeCoverage\Util\Xml;
-use SebastianBergmann\CodeCoverage\WriteOperationFailedException;
 
 final class Cobertura
 {
     /**
-     * @param null|non-empty-string $target
-     *
      * @throws WriteOperationFailedException
      */
     public function process(CodeCoverage $coverage, ?string $target = null): string
@@ -44,9 +44,10 @@ final class Cobertura
             'http://cobertura.sourceforge.net/xml/coverage-04.dtd',
         );
 
-        $document             = $implementation->createDocument('', '', $documentType);
-        $document->xmlVersion = '1.0';
-        $document->encoding   = 'UTF-8';
+        $document               = $implementation->createDocument('', '', $documentType);
+        $document->xmlVersion   = '1.0';
+        $document->encoding     = 'UTF-8';
+        $document->formatOutput = true;
 
         $coverageElement = $document->createElement('coverage');
 
@@ -152,7 +153,7 @@ final class Cobertura
 
                     $linesValid   = $method['executableLines'];
                     $linesCovered = $method['executedLines'];
-                    $lineRate     = $linesCovered / $linesValid;
+                    $lineRate     = $linesValid === 0 ? 0 : ($linesCovered / $linesValid);
 
                     $branchesValid   = $method['executableBranches'];
                     $branchesCovered = $method['executedBranches'];
@@ -227,7 +228,7 @@ final class Cobertura
 
                 $linesValid   = $function['executableLines'];
                 $linesCovered = $function['executedLines'];
-                $lineRate     = $linesCovered / $linesValid;
+                $lineRate     = $linesValid === 0 ? 0 : ($linesCovered / $linesValid);
 
                 $functionsLinesValid   += $linesValid;
                 $functionsLinesCovered += $linesCovered;
@@ -288,10 +289,16 @@ final class Cobertura
 
         $coverageElement->setAttribute('complexity', (string) $complexity);
 
-        $buffer = Xml::asString($document);
+        $buffer = $document->saveXML();
 
         if ($target !== null) {
-            Filesystem::write($target, $buffer);
+            if (!str_contains($target, '://')) {
+                Filesystem::createDirectory(dirname($target));
+            }
+
+            if (@file_put_contents($target, $buffer) === false) {
+                throw new WriteOperationFailedException($target);
+            }
         }
 
         return $buffer;
